e90f4cb0aca1c21bc74309de1c6c55b00826aafe
jeanpaul615 <jeanpaul.puerta@utp.edu.co>
2024-06-29 10:29:01 -0500

    Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,68 +1,127 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Home from './Home';
 
-export default function LoginForm({ username, password, setUsername, setPassword, handdleLogin }) {
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [loginSuccessful, setLoginSuccessful] = useState(false);
+
+    const handdleLogin = (e) =>{
+        e.preventDefault();
+        const data = {
+            username: username,
+            password: password
+        };
+        fetch('http://localhost:5000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response=> response.json())
+            .then(result => {
+                console.log(result.token)
+                if(result.token){
+                    localStorage.setItem('token', result.token)
+                    setLoginSuccessful(true);
+                } else {
+                    console.log("error de usuario")
+                    setLoginSuccessful(false);
+                }
+            })
+            .catch(error =>{
+                console.log(error)
+            })
+    }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // Lógica de autenticación (a implementar)
+      console.log(`Username: ${username}, Password: ${password}`);
+      
+      // Limpia los campos después del envío
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error.message);
+    }
+  };
+
   return (
-    <form className="mt-8 space-y-6" onSubmit={handdleLogin}>
-      <input type="hidden" name="remember" defaultValue="true" />
-      <div className="rounded-md shadow-sm -space-y-px">
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
         <div>
-          <label htmlFor="username" className="sr-only">Username</label>
-          <input
-            id="username"
-            name="username"
-            type="text"
-            autoComplete="username"
-            required
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Inicia sesión en tu cuenta</h2>
         </div>
-        <div>
-          <label htmlFor="password" className="sr-only">Password</label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            required
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-      </div>
+        {loginSuccessful ? <Home />:
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="username" className="sr-only">Username</label>
+              <input
+                id="username"
+                name="username"
+                type="text"
+                autoComplete="username"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">Password</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
 
-      <div className="flex items-center justify-between">
-        <div className="flex items-center">
-          <input
-            id="remember-me"
-            name="remember-me"
-            type="checkbox"
-            className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-          />
-          <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-            Recordarme
-          </label>
-        </div>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+              />
+              <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                Recordarme
+              </label>
+            </div>
 
-        <div className="text-sm">
-          <a href="/" className="font-medium text-indigo-600 hover:text-indigo-500">
-            ¿Olvidaste tu contraseña?
-          </a>
-        </div>
-      </div>
+            <div className="text-sm">
+              <a href="/" className="font-medium text-indigo-600 hover:text-indigo-500">
+                ¿Olvidaste tu contraseña?
+              </a>
+            </div>
+          </div>
 
-      <div>
-        <button
-          type="submit"
-          className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Iniciar sesión
-        </button>
+          <div>
+            <button
+              onClick={handdleLogin}
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Iniciar sesión
+            </button>
+          </div>
+        </form>}
       </div>
-    </form>
+    </div>
   );
 }
