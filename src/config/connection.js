bcb85a23ce66dcebb4245cc262c506e2a1e0820f
jeanpaul615 <jeanpaul.puerta@utp.edu.co>
2024-08-05 20:05:56 -0500

    conect

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,13 +1,33 @@
 const mysql = require('mysql');
 
+const mysql = require('mysql');
+
 const connection = mysql.createConnection({
-    host: '148.113.168.53',
-    user: 'legonfra_JeanPaulPuerta',
-    password: 'Allison2012@615',
-    database: 'legonfra_legon',
-    connectTimeout: 10000 // tiempo de espera en milisegundos
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'mydb'
 });
 
+function handleDisconnect() {
+  connection.connect(function(err) {
+    if (err) {
+      console.error('Error connecting to database:', err);
+      setTimeout(handleDisconnect, 2000); // Reintentar la conexión después de 2 segundos
+    }
+  });
+
+  connection.on('error', function(err) {
+    console.error('Database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect(); // Reconectar si la conexión se pierde
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 connection.connect((err) => {
     if (err) {
