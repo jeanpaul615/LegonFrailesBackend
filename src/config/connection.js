39188963dff5824701fd7bd4befee4d204f6001c
jeanpaul615 <jeanpaul.puerta@utp.edu.co>
2024-08-05 20:08:02 -0500

    finally

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,40 +1,35 @@
 const mysql = require('mysql');
 
-const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'mydb'
+    host: '148.113.168.53',
+    user: 'legonfra_JeanPaulPuerta',
+    password: 'Allison2012@615',
+    database: 'legonfra_legon',
+    connectTimeout: 10000 // tiempo de espera en milisegundos
 });
+
 
 function handleDisconnect() {
-  connection.connect(function(err) {
-    if (err) {
-      console.error('Error connecting to database:', err);
-      setTimeout(handleDisconnect, 2000); // Reintentar la conexión después de 2 segundos
-    }
-  });
-
-  connection.on('error', function(err) {
-    console.error('Database error:', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleDisconnect(); // Reconectar si la conexión se pierde
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleDisconnect();
-
-connection.connect((err) => {
-    if (err) {
-        console.error('Error conectando a la base de datos:', err.stack);
-        return;
-    }
-    console.log('Conectado a la base de datos con id', connection.threadId);
-});
-
-module.exports = connection;
+    connection.connect(function(err) {
+      if (err) {
+        console.error('Error connecting to database:', err);
+        setTimeout(handleDisconnect, 2000); // Reintentar la conexión después de 2 segundos
+      } else {
+        console.log('Conectado a la base de datos con id', connection.threadId);
+      }
+    });
+  
+    connection.on('error', function(err) {
+      console.error('Database error:', err);
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        handleDisconnect(); // Reconectar si la conexión se pierde
+      } else {
+        throw err;
+      }
+    });
+  }
+  
+  handleDisconnect();
+  
+  module.exports = connection;
