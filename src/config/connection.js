49cd61f3275190d3fba13c0c4b4bff3890708d69
Jean Paul <jeanpaul.puerta@utp.edu.co>
2024-08-07 18:55:04 -0500

    finalkdb

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,27 +1,18 @@
 const mysql = require('mysql');
 
-// Crear un pool de conexiones
-const pool = mysql.createPool({
-    connectionLimit: 10, // Número máximo de conexiones en el pool
+const connection = mysql.createConnection({
     host: '148.113.168.53',
     user: 'legonfra_JeanPaulPuerta',
     password: 'Allison2012@615',
     database: 'legonfra_legon'
 });
 
-// Función para obtener una conexión del pool
-const connection = (callback) => {
-    pool.getConnection((err, connection) => {
-        if (err) {
-            console.error('Error obteniendo conexión del pool:', err.stack);
-            callback(err, null);
-        } else {
-            callback(null, connection);
-        }
-    });
-};
+connection.connect((err) => {
+    if (err) {
+        console.error('Error conectando a la base de datos:', err.stack);
+        return;
+    }
+    console.log('Conectado a la base de datos con id', connection.threadId);
+});
 
-// Exportar la función para obtener conexiones del pool
-module.exports = {
-    connection
-};
+module.exports = connection;
