18cee05c059aa75a81f00b68bfa0429c2c2a977d
Jean Paul <jeanpaul.puerta@utp.edu.co>
2024-08-07 16:59:15 -0500

    change port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
 const app = express();
-const port = 3306;
+const port = 8000;
 const db = require('./src/config/connection'); // Asegúrate de que la ruta es correcta
 
 // Middleware
