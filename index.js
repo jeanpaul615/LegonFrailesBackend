4ce7a33a05cfac797a6e48bf5b57f19313fa0287
jeanpaul615 <127168141+jeanpaul615@users.noreply.github.com>
2024-08-10 14:16:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
 const app = express();
-const port = 8000;
+const port = 8001;
 const db = require('./src/config/connection'); // Asegúrate de que la ruta es correcta
 
 // Middleware
